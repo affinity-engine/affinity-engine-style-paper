833e4303a2cf5eda519844f1b50ffd6060e31c01
null null null <null@affinitygamescollective.org>
2017-09-13 13:18:53 -0400

    prevent default config conflicts with user config

--- addon/affinity-engine/configs/style-paper.js
+++ addon/affinity-engine/configs/style-paper.js
@@ -1,9 +1,11 @@
 export default {
   priority: 1.3,
-  prompt: {
-    attrs: {
-      classNames: {
-        decorative: ['ae-paper']
+  default: {
+    prompt: {
+      attrs: {
+        classNames: {
+          decorative: ['ae-paper']
+        }
       }
     }
   }
